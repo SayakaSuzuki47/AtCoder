19183b8b9f18930ba4cc3578835bf31dd4f67464
Sayaka Suzuki <s1suzukisayaka1s@gmail.com>
2025-04-12 20:23:28 +0900

    init file

---
+++ set_env_vars.sh
@@ -0,0 +1,13 @@
+# python3 3.11.4 -m venv atcoder
+
+# pip install --upgrade pip
+
+# library
+# https://img.atcoder.jp/file/language-update/language-list.html
+
+#atcoder
+# pip install online-judge-tools
+# pip install online-judge-template-generator
+
+# pip install numpy 
+# numpy==1.24.1
